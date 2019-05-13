8d05498bae7450e615a916574d16e5700a77dcb0
ahmedsallamdd <ahmedsallamdd@gmail.com>
2019-05-13 14:11:31 +0200

    init commit

---
+++ src/main/java/com/mycompany/driveeye/DriveEyeApplication.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.driveeye;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @author Sallam
+ */
+@SpringBootApplication  
+ public class DriveEyeApplication {  
+   public static void main(String[] args) throws Exception {  
+     SpringApplication.run(DriveEyeApplication.class, args);  
+   }  
+ }  
