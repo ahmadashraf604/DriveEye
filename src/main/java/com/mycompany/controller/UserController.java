e38e7245221eeff8f72f9a74ead66cfdadec7e5d
abdoawd <abdoawd@users.noreply.github.com>
2019-05-14 11:23:06 +0200

    pull

--- src/main/java/com/mycompany/controller/UserController.java
+++ src/main/java/com/mycompany/controller/UserController.java
@@ -8,7 +8,6 @@ package com.mycompany.controller;
 import com.mycompany.dao.UserDao;
 import com.mycompany.bean.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
--- src/main/java/com/mycompany/dao/seasonDao.java
+++ src/main/java/com/mycompany/dao/seasonDao.java
@@ -7,7 +7,6 @@ package com.mycompany.dao;
 
 import com.mycompany.bean.User;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 
 /**
