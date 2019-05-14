74a0c4bf63d7ba0eb8629f3295620f7ab27d87f4
ahmadashraf604 <ahmadashraf604@gmail.com>
2019-05-14 11:21:43 +0200

    edit in user controller

---
+++ src/main/java/com/mycompany/controller/Response.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.controller;
+
+/**
+ *
+ * @author Ashraf_R
+ */
+public class Response<T> {
+
+    private boolean status;
+    private T response;
+
+    public Response(boolean status, T response) {
+        this.status = status;
+        this.response = response;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public T getResponse() {
+        return response;
+    }
+
+    public void setResponse(T response) {
+        this.response = response;
+    }
+
+}
